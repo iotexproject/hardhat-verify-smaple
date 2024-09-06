db1ae9ff98617b3fca5aa62ff67a8877b2b2736a
zongzheng <304389312@qq.com>
2024-09-06 13:00:45 +0800

    modify some configurations
    Signed-off-by: zongzheng <304389312@qq.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,22 +5,19 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
-    iotex: {
-      chainId: 4689,
-      // from?: string;
-      // gas?: "auto" | number;
-      // gasPrice?: "auto" | number;
-      // gasMultiplier?: number;
-      url: 'https://babel-api.mainnet.iotex.io',
-      // timeout?: number;
-      // httpHeaders?: { [name: string]: string };
-      accounts: [
-        process.env.PRIVATE_KEY!
-      ]
+    mainnet: {
+      url: 'https://babel-api.mainnet.IoTeX.io',
+      accounts: [ process.env.PRIVATE_KEY! ],
+      chainId: 4689
+    },
+    testnet: {
+      url: 'https://babel-api.testnet.IoTeX.io',
+      accounts: [ process.env.PRIVATE_KEY! ],
+      chainId: 4690,
     }
   },
   etherscan: {
-    apiKey: "YOUR_ETHER",
+    apiKey: "arbitrary value",
     customChains: [
       {
         network: "mainnet",
